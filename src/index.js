162540c0a32d8f2cb0bba9eb4e8ed1c145092b93
BerdibekUalov <uralov0212@gmail.com>
2023-06-30 15:35:14 +0500

    full version

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from 'react-router-dom';
+
+import './index.scss';
+import App from './App';
+import 'macro-css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
