cf72f9339ee294bbc21afd8da1a7f467ffb397f3
BerdibekUalov <uralov0212@gmail.com>
2023-07-10 16:05:01 +0500

    version 1.21.03

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import axios from 'axios';
 
 import Card from '../components/Card';
-import AppContext from '../context';
 
 function Orders() {
-  const { onAddToFavorite, onAddToCart } = React.useContext(AppContext);
   const [orders, setOrders] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
